0b6b64f01be3125bc8015f285c9d2efd1e065bbf
Diurno <Diurno@www.logroniostimeMiguel.edu>
2024-04-09 14:06:05 +0200

    documentacion de curso

--- src/main/java/clases/Curso.java
+++ src/main/java/clases/Curso.java
@@ -2,28 +2,43 @@ package clases;
 
 import java.util.TreeSet;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Clase que representa un curso.
  */
- 
 public class Curso {
 
-     
     private String nombre;
     private TreeSet<Persona> listaAlumnos;
 
- 
-    protected String getNombre() {
-        return nombre;
-    }
- 
+    /**
+     * Constructor para crear un curso con un nombre.
+     * @param nombre El nombre del curso.
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
- 
+    /**
+     * Método para obtener el nombre del curso.
+     * @return El nombre del curso.
+     */
+    protected String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Método para añadir un alumno al curso.
+     * @param p El alumno a añadir.
+     */
+    public void añadirAlumno(Persona p) {
+        listaAlumnos.add(p);
+    }
+
+    /**
+     * Método para obtener una representación en cadena del curso.
+     * @return Una representación en cadena del curso, incluyendo los alumnos matriculados.
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -34,9 +49,5 @@ public class Curso {
         }
         return s;
     }
-
-   
-    public void añadirAlumno(Persona p) {
-        listaAlumnos.add(p);
-    }
 }
+
