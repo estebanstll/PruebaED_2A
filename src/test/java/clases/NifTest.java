b39af7e1bbad38c384678507e1dfd76e5ec359a3
Diurno <Diurno@www.logroniostimeMiguel.edu>
2024-04-09 13:56:26 +0200

    testeo de la clase NIF y documentacion de este

--- src/test/java/clases/NifTest.java
+++ src/test/java/clases/NifTest.java
@@ -3,24 +3,32 @@ package clases;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Clase de pruebas para la clase Nif.
+ */
 public class NifTest {
 
+    /**
+     * Constructor por defecto de NifTest.
+     */
     public NifTest() {
     }
 
     /**
-     * Test para calcularLetra
+     * Test para el método calcularLetra.
+     * Verifica si el método calcularLetra devuelve la letra esperada para un NIF dado.
      */
     @Test
     public void testCalcularLetra() {
         System.out.println("calcularLetra");
         assertEquals('S', Nif.calcularLetra(16666666));
-        assertEquals('Q', Nif.calcularLetra(16666667));
-//        assertEquals('W', Nif.calcularLetra(11111111));
+        assertEquals('Q', Nif.calcularLetra(16666667)); // El resultado esperado debe ser 'Q'
+        //assertEquals('W', Nif.calcularLetra(11111111)); // Puedes descomentar esta línea si deseas probar más casos
     }
 
     /**
-     * Test para el metodo toString
+     * Test para el método toString.
+     * Verifica si el método toString devuelve la representación de cadena esperada para un objeto Nif.
      */
     @Test
     public void testToString() {
@@ -30,7 +38,8 @@ public class NifTest {
     }
 
     /**
-     * Test para e metodo equals
+     * Test para el método equals.
+     * Verifica si el método equals compara adecuadamente las instancias de Nif.
      */
     @Test
     public void testEquals() {
@@ -42,4 +51,5 @@ public class NifTest {
         assertFalse(instance1.equals(instance3));
     }
 }
+
 
