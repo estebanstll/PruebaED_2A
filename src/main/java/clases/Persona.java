9bc113fb5bdf4fef5e9dc90a534edc4ae5de3be2
Diurno <Diurno@www.logroniostimeMiguel.edu>
2024-04-09 14:11:55 +0200

    documentacion de Persona

--- src/main/java/clases/Persona.java
+++ src/main/java/clases/Persona.java
@@ -1,13 +1,12 @@
 package clases;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Objects;
 
+/**
+ * Clase que representa a una persona.
+ */
 public class Persona implements Comparable<Persona> {
 
     private Nif nif;
@@ -15,74 +14,132 @@ public class Persona implements Comparable<Persona> {
     private char genero;
     private LocalDate nacimiento;
 
- 
+    /**
+     * Constructor por defecto de Persona.
+     * Crea una persona con valores predeterminados.
+     */
     public Persona() {
         nif = new Nif();
         nombre = "";
         genero = ' ';
         nacimiento = LocalDate.of(1990, 1, 1);
     }
- 
+
+    /**
+     * Constructor para crear una persona con un NIF dado.
+     * @param nif El NIF de la persona.
+     */
     public Persona(int nif) {
         this();
         this.nif = new Nif(nif);
     }
- 
-    public Persona(int nif, String nombre, char genero,
-            int dia, int mes, int ano) {
+
+    /**
+     * Constructor para crear una persona con un NIF, nombre, género y fecha de nacimiento dados.
+     * @param nif El NIF de la persona.
+     * @param nombre El nombre de la persona.
+     * @param genero El género de la persona.
+     * @param dia El día de nacimiento de la persona.
+     * @param mes El mes de nacimiento de la persona.
+     * @param ano El año de nacimiento de la persona.
+     */
+    public Persona(int nif, String nombre, char genero, int dia, int mes, int ano) {
         this.nif = new Nif(nif);
         this.nombre = nombre;
         this.genero = genero;
-        this.nacimiento
-                = LocalDate.of(ano, mes, dia);
+        this.nacimiento = LocalDate.of(ano, mes, dia);
     }
- 
+
+    /**
+     * Obtiene el NIF de la persona.
+     * @return El NIF de la persona.
+     */
     public Nif getNif() {
         return nif;
     }
- 
+
+    /**
+     * Establece el NIF de la persona.
+     * @param nif El NIF de la persona.
+     */
     public void setNif(Nif nif) {
         this.nif = nif;
     }
- 
+
+    /**
+     * Obtiene el nombre de la persona.
+     * @return El nombre de la persona.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre de la persona.
+     * @param nombre El nombre de la persona.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
- 
+
+    /**
+     * Obtiene el género de la persona.
+     * @return El género de la persona.
+     */
     public char getGenero() {
         return genero;
     }
 
+    /**
+     * Establece el género de la persona.
+     * @param genero El género de la persona.
+     */
     public void setGenero(char genero) {
         this.genero = genero;
     }
- 
+
+    /**
+     * Obtiene la fecha de nacimiento de la persona.
+     * @return La fecha de nacimiento de la persona.
+     */
     public LocalDate getNacimiento() {
         return nacimiento;
     }
- 
+
+    /**
+     * Establece la fecha de nacimiento de la persona.
+     * @param nacimiento La fecha de nacimiento de la persona.
+     */
     public void setNacimiento(LocalDate nacimiento) {
         this.nacimiento = nacimiento;
     }
 
+    /**
+     * Obtiene la edad de la persona.
+     * @return La edad de la persona.
+     */
     public int getEdad() {
         return Period.between(nacimiento, LocalDate.now()).getYears();
     }
- 
+
+    /**
+     * Devuelve una representación en cadena de la persona.
+     * @return La representación en cadena de la persona.
+     */
     @Override
     public String toString() {
         if (nombre.split(" ").length > 1) {
-            return nif + "\t" + nombre.split(" ")[0]
-                    + '\t' + nombre.split(" ")[1] + "\t\t" + getEdad();
+            return nif + "\t" + nombre.split(" ")[0] + '\t' + nombre.split(" ")[1] + "\t\t" + getEdad();
         } else {
             return nif + "\t" + nombre + "\t\t\t" + getEdad();
         }
     }
- 
+
+    /**
+     * Compara si esta persona es igual a otra persona.
+     * @param a La persona con la que se compara.
+     * @return true si las personas son iguales, false en caso contrario.
+     */
     public boolean equals(Persona a) {
         if (a == null) {
             return false;
@@ -90,8 +147,11 @@ public class Persona implements Comparable<Persona> {
         return a.nif.toString().equals(this.nif.toString());
     }
 
- 
-    
+    /**
+     * Compara si esta persona es igual a otro objeto.
+     * @param obj El objeto con el que se compara.
+     * @return true si los objetos son iguales, false en caso contrario.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -108,9 +168,15 @@ public class Persona implements Comparable<Persona> {
         return Objects.equals(this.nif, other.nif);
     }
 
+    /**
+     * Compara esta persona con otra persona para determinar el orden relativo.
+     * @param o La persona con la que se compara.
+     * @return Un valor negativo si esta persona es anterior a la otra,
+     *         cero si son iguales y un valor positivo si esta persona es posterior a la otra.
+     */
     @Override
     public int compareTo(Persona o) {
         return this.nif.toString().compareTo(o.nif.toString());
     }
-
 }
+
